d5f8de262c36f81bf9858cc011bb4bf5c5107ae7
Eden Xu <eden.xu@uptickhq.com>
2025-04-24 16:49:52 +1000

    build: dockerise frontend backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
-
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.tsx'
+      }
+    }
+  }
 })
