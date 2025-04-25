1afdd18e0c0129642044bb97abbf4fd84e3f8618
kraklo <evanliapakis@gmail.com>
2025-04-25 15:09:12 +1000

    add player updates

--- backend/src/game.rs
+++ backend/src/game.rs
@@ -1,19 +1,102 @@
 use rocket::serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Clone)]
+pub const TICKS_PER_SECOND: f64 = 1_f64;
+
+#[derive(Serialize, Clone)]
 #[serde(crate = "rocket::serde")]
 pub struct Game {
-    tick: i64,
+    player_one: PlayerState,
+    player_two: PlayerState,
 }
 
 impl Game {
-    pub fn update(&mut self) {
-        self.tick += 1;
+    pub fn update(&mut self) {}
+    pub fn client_update(&mut self, payload: UpdatePayload) {
+        let scaled_vector = payload.velocity * (1_f64 / TICKS_PER_SECOND);
+
+        if payload.player_id == "1" {
+            self.player_one.position += scaled_vector;
+        } else if payload.player_id == "2" {
+            self.player_two.position += scaled_vector;
+        }
     }
 }
 
 impl Default for Game {
     fn default() -> Self {
-        Game { tick: 0 }
+        Self {
+            player_one: PlayerState {
+                id: String::from("1"),
+                position: Vector3::default(),
+            },
+            player_two: PlayerState {
+                id: String::from("2"),
+                position: Vector3::default(),
+            },
+        }
     }
+}
+
+#[derive(Serialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct PlayerState {
+    id: String,
+    position: Vector3,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+struct Vector3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Default for Vector3 {
+    fn default() -> Self {
+        Self {
+            x: 0_f64,
+            y: 0_f64,
+            z: 0_f64,
+        }
+    }
+}
+
+impl std::ops::Add for Vector3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl std::ops::AddAssign for Vector3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x = self.x + rhs.x;
+        self.y = self.y + rhs.y;
+        self.z = self.z + rhs.z;
+    }
+}
+
+impl std::ops::Mul<f64> for Vector3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+#[derive(Deserialize, Clone)]
+#[serde(crate = "rocket::serde")]
+pub struct UpdatePayload {
+    player_id: String,
+    velocity: Vector3,
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,24 @@
 mod game;
 
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::thread::sleep;
-use std::time::{Duration, SystemTime};
+use std::sync::Arc;
 
-use game::Game;
+use game::{Game, UpdatePayload};
+use rocket::tokio;
+use rocket::tokio::time::{interval, Duration};
 use rocket::{
-    futures::{stream::FusedStream, SinkExt},
+    futures::{lock::Mutex, SinkExt, StreamExt},
     http::Method,
     State,
 };
-use ws;
+use ws::{self, Message};
 
 use rocket_cors::{AllowedHeaders, AllowedOrigins, CorsOptions};
 
 #[macro_use]
 extern crate rocket;
 
-const TICKS_PER_SECOND: f64 = 1_f64;
-
-#[launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     let allowed_origins = AllowedOrigins::some_exact(&[
         "http://127.0.0.1:5173",
         "http://localhost:5173",
@@ -49,38 +46,35 @@ fn rocket() -> _ {
     .expect("CORS fairing creation failed");
 
     let game_state = Arc::new(Mutex::new(Game::default()));
+
+    // Creates an index route, mounts the route at / and launches the app
+    let r = rocket::build()
+        .mount("/", routes![index, hello, game_stream])
+        .attach(cors)
+        .manage(Arc::clone(&game_state));
+
     let game_state_ref = Arc::clone(&game_state);
 
     // Game loop thread
     // Updates 120 times/sec
-    thread::spawn(move || {
-        let tick_duration = Duration::from_secs_f64(1_f64 / TICKS_PER_SECOND);
-        let mut last_update_time = SystemTime::now();
+    tokio::spawn(async move {
+        let tick_duration = Duration::from_secs_f64(1_f64 / game::TICKS_PER_SECOND);
+        let mut tick_interval = interval(tick_duration);
 
         loop {
             // block for game state updates so lock drops afterwards
             {
-                let mut thread_game_state = game_state_ref.lock().unwrap();
+                let mut thread_game_state = game_state_ref.lock().await;
                 thread_game_state.update();
             }
 
-            let now = SystemTime::now();
-            let duration = now.duration_since(last_update_time).unwrap();
-
-            if duration < tick_duration {
-                let diff = tick_duration.abs_diff(duration);
-                sleep(diff);
-            }
-
-            last_update_time = SystemTime::now();
+            tick_interval.tick().await;
         }
     });
 
-    // Creates an index route, mounts the route at / and launches the app
-    rocket::build()
-        .mount("/", routes![index, hello, game_stream])
-        .attach(cors)
-        .manage(game_state)
+    r.launch().await?;
+
+    Ok(())
 }
 
 #[get("/")]
@@ -97,27 +91,51 @@ fn hello(name: &str) -> String {
 fn game_stream<'r>(ws: ws::WebSocket, state: &'r State<Arc<Mutex<Game>>>) -> ws::Channel<'r> {
     ws.channel(move |mut stream| {
         Box::pin(async move {
-            let tick_duration = Duration::from_secs_f64(1_f64 / TICKS_PER_SECOND);
-            let mut last_update_time = SystemTime::now();
+            let tick_duration = Duration::from_secs_f64(1_f64 / game::TICKS_PER_SECOND);
+            let mut tick_interval = interval(tick_duration);
+            let state_ref = state.inner().clone();
 
-            while !stream.is_terminated() {
-                let game_state = state.lock().unwrap().clone();
-                let _ = stream
-                    .send(ws::Message::text(
-                        serde_json::to_string(&game_state).unwrap(),
-                    ))
-                    .await;
-
-                let now = SystemTime::now();
-                let duration = now.duration_since(last_update_time).unwrap();
-
-                if duration < tick_duration {
-                    let diff = tick_duration.abs_diff(duration);
-                    sleep(diff);
+            tokio::spawn(async move {
+                loop {
+                    tokio::select! {
+                        _ = tick_interval.tick() => {
+                            let game_state = state_ref.lock().await;
+                            let _ = stream
+                                .send(ws::Message::text(
+                                    serde_json::to_string(&game_state.clone()).unwrap(),
+                                ))
+                                .await;
+                        }
+                        Some(Ok(message)) = stream.next() => {
+                            match message {
+                                Message::Text(text) => {
+                                    let mut game_state = state_ref.lock().await;
+                                    let payload: UpdatePayload =
+                                        serde_json::from_str(text.as_str()).unwrap();
+                                    game_state.client_update(payload);
+                                }
+                                Message::Close(_) => {
+                                    let close_frame = ws::frame::CloseFrame {
+                                        code: ws::frame::CloseCode::Normal,
+                                        reason: "Client disconected".to_string().into(),
+                                    };
+                                    let _ = stream.close(Some(close_frame)).await;
+                                    break;
+                                }
+                                _ => {}
+                            }
+                        }
+                        else => {
+                            let close_frame = ws::frame::CloseFrame {
+                                code: ws::frame::CloseCode::Normal,
+                                reason: "Client disconected".to_string().into(),
+                            };
+                            let _ = stream.close(Some(close_frame)).await;
+                            break;
+                        }
+                    }
                 }
-
-                last_update_time = SystemTime::now();
-            }
+            });
 
             Ok(())
         })
