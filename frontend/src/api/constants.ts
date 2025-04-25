6545ff8ea33184dcda47f25118d5c9ad5e207c6b
Eden Xu <eden.xu@uptickhq.com>
2025-04-25 19:06:36 +1000

    build: conditionally choose url based on env

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,3 +1,6 @@
+const ENV = import.meta.env.MODE; // 'development' or 'production'
+
 export const BASE_URL = "http://0.0.0.0:8000/";
 export const DEPLOYED_URL = "https://codebrew-2025-backend.onrender.com/";
-export const WS_URL = `${BASE_URL}game_state`;
+
+export const WS_URL = `${ENV === "production" ? DEPLOYED_URL : BASE_URL}game_state`;
