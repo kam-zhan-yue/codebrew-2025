72aa983d3c3b2c038c7e5219a2a92a409fb73880
kamzhanyue <kamzhanyue@gmail.com>
2025-04-26 13:09:30 +1000

    chore: room 1

--- frontend/src/game/components/level.tsx
+++ frontend/src/game/components/level.tsx
@@ -1,11 +1,11 @@
 import Player from "./player";
-import { EnvironmentModel } from "../models/environment-model";
 import Gameboy from "./gameboy";
+import { RoomModel } from "../models/room-model";
 
 const Level = () => {
   return (
     <>
-      <EnvironmentModel />
+      <RoomModel />
       <Gameboy position={[0, 1, 0]} rotation={[0, 0, 0]} />
       <Player />
     </>
