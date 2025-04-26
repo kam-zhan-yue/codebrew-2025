9c39eafb7182455e081d25168f7c6bb197d42ce3
Eden Xu <eden.xu@uptickhq.com>
2025-04-26 16:57:54 +1000

    fix: fix boombox sound

--- frontend/src/game/components/boombox.tsx
+++ frontend/src/game/components/boombox.tsx
@@ -3,6 +3,9 @@ import { GameFlow, useGameStore } from "../../store";
 import { Interactions } from "../types/interactions";
 import Tooltip from "./tooltip";
 import { BoomboxModel } from "../models/boombox-model";
+import { useCallback, useEffect, useRef } from "react";
+import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
 
 interface BoomboxProps {
   position: [number, number, number];
@@ -11,6 +14,8 @@ interface BoomboxProps {
 }
 
 const Boombox = ({ position, rotation, scale }: BoomboxProps) => {
+  const { camera } = useThree();
+  const playing = useRef<boolean>(false);
   const interactions = useGameStore((s) => s.gameState.interactions);
   const flow = useGameStore((s) => s.flow);
   const boombox = interactions?.find(
@@ -19,6 +24,49 @@ const Boombox = ({ position, rotation, scale }: BoomboxProps) => {
   const activeSelection = useGameStore(
     (s) => s.uiState.selection.activeSelection,
   );
+
+  const musicSoundRef = useRef<THREE.Audio | null>(null);
+
+  useEffect(() => {
+    const audioLoader = new THREE.AudioLoader();
+    const listener = new THREE.AudioListener();
+    const musicSound = new THREE.Audio(listener);
+    audioLoader.load("/sounds/rickroll.mp3", (buffer) => {
+      musicSound.setBuffer(buffer);
+      musicSound.setVolume(0.5);
+      musicSound.setLoop(true);
+      musicSoundRef.current = musicSound; // Store the sound in the ref
+    });
+    camera.add(listener)
+  }, [camera])
+
+  const playMusic = useCallback(() => {
+    if (musicSoundRef.current) {
+      musicSoundRef.current?.play();
+    }
+  }, [])
+
+  const stopMusic = useCallback(() => {
+    if (musicSoundRef.current) {
+      musicSoundRef.current?.pause();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (boombox) {
+      const active = boombox.active;
+      // If the boombox turned active and we are not playing, then play
+      if (active && !playing.current) {
+        console.info("Play music")
+        playing.current = true;
+        playMusic();
+      } else if (!active && playing.current) {
+        console.info("Stop music")
+        playing.current = false;
+        stopMusic();
+      }
+    }
+  }, [boombox, playMusic, stopMusic]);
 
   if (!boombox) {
     return <></>;
@@ -32,6 +80,7 @@ const Boombox = ({ position, rotation, scale }: BoomboxProps) => {
   } else {
     message = data.description;
   }
+
 
   return (
     <Select enabled={isHovering}>
