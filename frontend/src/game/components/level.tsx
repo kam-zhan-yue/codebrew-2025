18fbb61147e0efe238b0be44cb0d875897d48754
kamzhanyue <kamzhanyue@gmail.com>
2025-04-26 15:19:43 +1000

    feat: add phone

--- frontend/src/game/components/level.tsx
+++ frontend/src/game/components/level.tsx
@@ -3,6 +3,7 @@ import Gameboy from "./gameboy";
 import { RoomModel } from "../models/room-model";
 import Book from "./book";
 import Ds from "./ds";
+import Phone from "./phone";
 
 const Level = () => {
   return (
@@ -10,6 +11,7 @@ const Level = () => {
       <RoomModel position={[0, 0, 0]} scale={0.5} />
       <Book position={[2.3, 1.2, 4.4]} rotation={[0, 0, 0]} scale={0.5} />
       <Ds position={[2.8, 1.6, 0]} rotation={[0, 0, 0]} scale={0.5} />
+      <Phone position={[-0.2, 1.2, 5.1]} rotation={[0, 0, 0]} scale={0.5} />
       <Gameboy position={[0, 1, 0]} rotation={[0, 0, 0]} />
       <Player />
     </>
--- frontend/src/game/components/phone.tsx
+++ frontend/src/game/components/phone.tsx
@@ -2,14 +2,16 @@ import { Select } from "@react-three/postprocessing";
 import { GameFlow, useGameStore } from "../../store";
 import { Interactions } from "../types/interactions";
 import Tooltip from "./tooltip";
-import { Vector3 } from "three";
+import { PhoneInactiveModel } from "../models/phone-inactive-model";
+import { PhoneActiveModel } from "../models/phone-active-model";
 
 interface PhoneProps {
-  position: Vector3;
+  position: [number, number, number];
   rotation: [number, number, number];
+  scale: number;
 }
 
-const Phone = ({ position, rotation }: PhoneProps) => {
+const Phone = ({ position, rotation, scale }: PhoneProps) => {
   const interactions = useGameStore((s) => s.gameState.interactions);
   const flow = useGameStore((s) => s.flow);
   const interaction = interactions?.find(
@@ -34,16 +36,14 @@ const Phone = ({ position, rotation }: PhoneProps) => {
     message = data.description;
   }
 
-  // console.info("Active Selection ", activeSelection, " ID ", phone.id);
-
   return (
     <Select enabled={isHovering}>
-      <group position={position} rotation={rotation}>
+      <group position={position} rotation={rotation} scale={scale}>
         <group name={interaction.id}>
-          {/* {interaction.active && <PhoneActiveModel />}
-          {!interaction.active && <PhoneInactiveModel />} */}
+          {interaction.active && <PhoneActiveModel />}
+          {!interaction.active && <PhoneInactiveModel />}
         </group>
-        {isHovering && <Tooltip position={[0, 5, 0]}>{message}</Tooltip>}
+        {isHovering && <Tooltip position={[0, 0.5, 0]}>{message}</Tooltip>}
       </group>
     </Select>
   );
