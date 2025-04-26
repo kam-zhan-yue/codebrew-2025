ebca8b7697a7f9aa240c9a855669521a20698ad8
Eden Xu <eden.xu@uptickhq.com>
2025-04-26 17:36:06 +1000

    feat: start screen

--- frontend/src/components/player-selection.tsx
+++ frontend/src/components/player-selection.tsx
@@ -14,22 +14,29 @@ const PlayerSelection = () => {
   };
 
   return (
-    <Overlay className="fixed inset-10 flex items-end justify-center pb-8 pointer-events-none">
-      <div className="flex gap-4 pointer-events-auto">
-        <button
-          onClick={handlePlayerOne}
-          className="px-6 py-3 text-white text-lg bg-blue-500 rounded-md"
-        >
-          Player One
-        </button>
-        <button
-          onClick={handlePlayerTwo}
-          className="px-6 py-3 text-white text-lg bg-green-500 rounded-md"
-        >
-          Player Two
-        </button>
-      </div>
-    </Overlay>
+    <>
+      <Overlay className="fixed flex h-90 justify-center pointer-events-none">
+        <h1 style={{fontSize: "150px", border: "white"}}>RETRONAUTS</h1>
+      </Overlay>
+      <Overlay className="fixed inset-10 flex items-end justify-center pb-70 pointer-events-none">
+        <div className="flex gap-4 pointer-events-auto">
+          <button
+            type="button"
+            onClick={handlePlayerOne}
+            className="px-6 py-3 text-white text-lg bg-red-500 rounded-md"
+          >
+            Player Red
+          </button>
+          <button
+            type="button"
+            onClick={handlePlayerTwo}
+            className="px-6 py-3 text-white text-lg bg-blue-500 rounded-md"
+          >
+            Player Blue
+          </button>
+        </div>
+      </Overlay>
+    </>
   );
 };
 
